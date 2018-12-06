39ca9dce1dd2dba5009c77f5f7a50281cceba846
Leonid Malinovsky <you@example.com>
2018-12-06 18:37:40 +0300

    global fix0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,62 @@
-'use strict';
+'use strict'
 
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const clean = require('gulp-clean');
-const sourcemaps = require('gulp-sourcemaps');
-const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
-const cssnano = require('gulp-cssnano');
-const runSequence = require('run-sequence');
-const pug = require('gulp-pug');
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const clean = require('gulp-clean')
+const sass = require('gulp-sass')
+const minify = require('gulp-uglify')
+const autoprefixer = require('gulp-autoprefixer')
+const concat = require('gulp-concat')
+const babel = require('gulp-babel')
+const gulpSequence = require('gulp-sequence')
+const pug = require('gulp-pug')
 
-const path = {
-    dist:{
-        server:         './dist',
-        css:            './dist/css',
-    },
-    src:{
-        scss:           './src/scss/main.scss',
-        scss_modules:   './src/scss/modules/**/*.scss',
-        pug:            './src/pug/newToDoList.pug',
-    },
-    watch:{
-        style:          'src/scss/**/*.scss',
-        pug:            'src/pug/**/*.pug',
-        layout:         'src/layout/**/*.pug'
-    }
-};
+gulp.task('clean', () =>
+ 	gulp.src('./dist', {
+      read: false
+    })
+    .pipe(clean())
+)
 
-gulp.task('pug', function () {
-    return gulp.src(path.src.pug)
+gulp.task('pug', () => {
+    gulp.src('./src/pug/index.pug')
         .pipe(pug({
             pretty: true
         }))
-        .pipe(gulp.dest(path.dist.server)); // указываем gulp куда положить скомпилированные HTML файлы
-});
+        .pipe(gulp.dest('./dist')) 
+})
 
-gulp.task('css', function () {
-    return gulp.src(path.src.scss)
-        .pipe(plumber()) // Отслеживание ошибок
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole: true,
-            outputStyle: 'expanded' // Добавление отступов между классами в итоговых стилях
-        }))
-        .pipe(autoprefixer({ // Добавление префиксов
-            browsers: ['last 5 versions'],
-            cascade: false
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(rename("main.css")) // Изменение название с расширением css
-        .pipe(gulp.dest(path.dist.css))
-        .pipe(cssnano({ // Сжатие css файла
-            zindex: false
-        }))
-        .pipe(rename({ suffix: '.min' })) // Добавление к css файлу суфикса min
-        .pipe(gulp.dest(path.dist.css));
-});
+gulp.task('minjs', () =>
+ gulp.src('./src/js/**/*.js')
+ 	.pipe(babel({
+  	presets: ['@babel/env']
+  }))
+  .pipe(minify())
+  .pipe(concat('script.min.js'))
+  .pipe(gulp.dest('./dist/js'))
+)
 
-gulp.task('clean', function () {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
-});
+gulp.task('sass', () =>
+ gulp.src('./src/scss/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+	    browsers: ['last 2 versions'],
+	    cascade: false
+		}))
+		.pipe(concat('style.min.css'))
+		.pipe(gulp.dest('./dist/css'))
+)
 
-gulp.task('build', function () {
-    runSequence('clean', ['pug', 'css']);
-});
+gulp.task('build', gulpSequence('clean', ['pug', 'sass', 'minjs']))
 
-gulp.task('dev', function (){
-    runSequence('clean', ['pug', 'css'], function(){ // выполняет указанные таски по очереди
-        browserSync.init({
-            server: path.dist.server
-        });
-        gulp.watch(path.watch.style,['css']).on('change', browserSync.reload);
-        gulp.watch(path.watch.pug, ['pug']).on('change', browserSync.reload);
-        gulp.watch(path.watch.layout, ['pug']).on('change', browserSync.reload);
-    })
-});
+gulp.task('devserver', ['build'], () => {
+	browserSync.init({
+    server: "./dist"
+	})
+    gulp.watch('./src/pug/**/*.pug', ['pug']).on('change', browserSync.reload)
+    gulp.watch('./src/layout/**/*.pug', ['pug']).on('change', browserSync.reload)
+    gulp.watch('./src/js/**/*.js', ['minjs']).on('change', browserSync.reload)
+    gulp.watch('./src/scss/**/*.scss', ['sass']).on('change', browserSync.reload)
+})
+
+gulp.task('dev', gulpSequence('clean', 'devserver'))
